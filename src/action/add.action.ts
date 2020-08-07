d4fa08c731e7a17bec0e3c647f95dcfe3f9f92fc
Avinash <avinash@Avinashs-MacBook-Pro.local>
2020-08-07 17:29:40 +0530

    feat(commit:linting): commit linting added

--- src/action/add.action.ts
+++
@@ -1,6 +0,0 @@
-import {createAction} from '@reduxjs/toolkit';
-
-/**
- * Action to repond when fetchCanJobApplyFailed action is failed.
- */
-export const add = createAction<null, string>('[Add] Add Something');
